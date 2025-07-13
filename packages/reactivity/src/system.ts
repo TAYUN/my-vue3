7d7f17d3824d487008c8b5ac5e0402289d42215c
tangyy <507028106@qq.com>
2025-07-13 16:57:36 +0800

    feat(reactivity): 实现订阅者和依赖项的双向关联
    - 新增 Dep 和 Sub 接口，用于表示依赖项和订阅者
    - 在 Link 接口中添加 dep 和 nextDep 属性，用于建立双向关联
    - 重构 link 函数，实现订阅者和依赖项的双向链表关联
    - 优化了关联逻辑，支持复用现有节点，避免重复建立关联

--- packages/reactivity/src/system.ts
+++ packages/reactivity/src/system.ts
@@ -1,10 +1,28 @@
-import { ReactiveEffect } from 'vue'
+// import { ReactiveEffect } from 'vue'
+
+interface Dep {
+  // 订阅者链表的头节点
+  subs: Link | undefined
+  // 订阅者链表的尾节点
+  subsTail: Link | undefined
+}
+
+interface Sub {
+  // 依赖项链表的头结点
+  deps: Link | undefined
+  // 依赖项链表的尾节点
+  depsTail: Link | undefined
+}
 /**
  * 链表节点
  */
 export interface Link {
   // 保存effect
-  sub: ReactiveEffect
+  sub: Sub
+  // 节点的依赖项 ref computed
+  dep: Dep
+  // 下一个依赖项节点
+  nextDep: Link | undefined
   // 上一个节点
   nextSub: Link | undefined
   // 下一个节点
@@ -17,11 +35,40 @@ export interface Link {
  * @param sub
  */
 export function link(dep, sub) {
+  // 如果 dep 和 sub 已经建立关系，则不需要建立关系
+  const currentDep = sub.depsTail
+
+  /**
+   * 复用节点两种情况
+   * 1. dep 和 sub 已经建立关系，则不需要建立关系, 即 sub.depsTail没有，并且sub.deps.dep === dep
+   * 2. 尾节点有 nextDep ，这种情况要复用尾节点的nextDep
+   */
+  // if(currentDep === undefined && sub.deps){
+  //   // 尾节点没有，头结点有
+  //   if(sub.deps.dep === dep){
+  //     sub.depsTail = sub.deps
+  //     return
+  //   }
+  // } else if(currentDep){
+  //   if(currentDep.nextDep?.dep === dep){
+  //     sub.depsTail = currentDep.nextDep
+  //     return
+  //   }
+  // }
+  const nextDep = currentDep === undefined ? sub.deps : currentDep.nextDep
+  if (nextDep && nextDep.dep === dep) {
+    sub.depsTail = nextDep
+    return
+  }
+
   const newLink = {
     sub,
+    dep,
+    nextDep: undefined,
     nextSub: undefined,
     preSub: undefined,
   }
+  // region 将链表节点和dep建立关联关系
   /**
    * 添加到链表末尾
    * 1. 如果有尾节点，将newLink设置为尾节点的nextSub
@@ -35,6 +82,22 @@ export function link(dep, sub) {
     dep.subs = newLink
     dep.subsTail = newLink
   }
+  // endregion
+
+  // region 将链表节点和sub建立关联关系
+  /**
+   * 关联链表关系，分两种情况
+   * 1. 如果有尾节点，将newLink设置为尾节点的nextDep
+   * 2. 否者尾节点为空，则将newLink设置为头节点，尾节点也为newLink
+   */
+  if (sub.depsTail) {
+    sub.depsTail.nextDep = newLink
+    sub.depsTail = newLink
+  } else {
+    sub.deps = newLink
+    sub.depsTail = newLink
+  }
+  // endregion
 }
 
 /**
