f365677f3c328ca5603ca2e1592f0a3ebefd5c7f
TAYUN <80144903+TAYUN@users.noreply.github.com>
2025-09-17 15:25:05 +0800

    feat(reactivity): 添加 hasChange 函数
    - 新增 hasChange 函数，用于判断新旧值是否发生变化
    - 优化 isObject 函数，提高对象判断的准确性

--- packages/reactivity/node_modules/@vue/.ignored_shared/src/index.ts
+++ packages/reactivity/node_modules/@vue/.ignored_shared/src/index.ts
@@ -2,3 +2,13 @@
 export function isObject(obj: any): boolean {
   return obj && typeof obj === 'object' && !Array.isArray(obj) && obj !== null
 }
+
+/**
+ * 判断两个值是否相等，有没有改变
+ * @param newValue 新值
+ * @param oldValue 老值
+ * @returns { boolean }
+ */
+export function hasChange(newValue, oldValue ) {
+  return !Object.is(newValue, oldValue)
+}
